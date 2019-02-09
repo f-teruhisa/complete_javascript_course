bac33db552d5711715a661a5fa0099c3638b575e
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-02-09 21:31:18 +0900

    add boolean operator

--- 02_js_basic/script.js
+++ 02_js_basic/script.js
@@ -55,6 +55,7 @@ console.log(firstName + ' ' + lastName);
 /*
 * Basic operators
 */
+/*
 var year, yearJohn, yearMark;
 now = 2018;
 ageJohn = 28;
@@ -80,3 +81,75 @@ console.log(typeof ageJohn);
 console.log(typeof 'Mark is older than John');
 var c;
 console.log(typeof x);
+*/
+
+/*
+* Operator precedence
+*/
+
+/*
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Multiple operators
+var isFullAge = now - yearJohn >= fullAge; // true
+console.log(isFullAge);
+
+
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+// Multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6;
+console.log(x, y);
+
+// More operators
+x *= 2;
+console.log(x);
+x *= 10
+console.log(x);
+x++; // same as x += 1; & x = x +1
+
+*/
+
+/*
+* If / else statement
+*/
+
+/*
+var firstName = 'John';
+var civilStatus = 'single';
+
+if (civilStatus === 'married') {
+  console.log(firstName + ' is mariied!');
+} else {
+  console.log(firstName + ' will hopefully maeey soon :');
+}
+
+var isMarried = true;
+if (isMarried === true) {
+  console.log(firstName + ' is mariied!');
+} else {
+  console.log(firstName + ' will hopefully maeey soon :');
+}
+*/
+
+/*
+* Boolean Logic
+*/
+
+var firstName = "John";
+var age = 16;
+
+if (age < 13) {
+  console.log(firstName + ' is a boy.');
+} else if (age >= 13 && age < 20) {
+  console.log(firstName + ' is a teenager.');
+} else {
+  console.log(firstName + ' is a man.');
+}
