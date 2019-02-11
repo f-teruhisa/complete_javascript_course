b0df6d155e9bb94536b39c22e153b26f8ed1b1f2
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-02-11 14:27:37 +0900

    hoisting lecture

--- 03_how_JS_works/script.js
+++ 03_how_JS_works/script.js
@@ -1,17 +1,31 @@
 ///////////////////////////////////////
-// Lecture: Hoisting
+// Lecture: Hoisting(巻き上げ)
 
+calculateAge(1965);
 
+function calculateAge(year) {
+  console.log(2018 - year);
+}
 
+// hostingは変数ではなくfunctionの呼び出しでのみ起こるため、こちらは巻き上げができない
+// retirement(1990);
 
+var retirement = function (year) {
+  console.log(65 - (2016 - year));
+}
 
+// variables declearation(変数宣言の場合、巻き上げはできない)
 
+console.log(age);
+var age = 23;
 
-
-
-
-
-
+function foo() {
+  console.log(age); // undifined
+  var age = 65;
+  console.log(age);
+}
+foo(); // age = 65(local variable)
+console.log(age); // age = 23(global variable)
 
 
 
