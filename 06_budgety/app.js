164d568d1ef878741c000ca20946cd0f0697854a
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-03-02 20:30:19 +0900

    Adding a New Item to Our Budget Controller

--- 06_budgety/app.js
+++ 06_budgety/app.js
@@ -25,6 +25,40 @@ var budgetController = (function () {
       exp: 0,
       inc: 0
     }
+  };
+
+  return {
+    addItem: function (type, des, val) {
+      var newItem, ID;
+
+      // [1 2 3 4 5], next ID = 6
+      // [1 2 4 6 8], next ID = 9
+      // ID = last ID + 1
+
+      // Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Create nreew item based on 'inc' or 'exp' type
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+
+      // Push it into our data structure
+      data.allItems[type].push(newItem);
+
+      // Return the new element
+      return newItem;
+    },
+
+    testing: function () {
+      console.log(data);
+    }
   }
 })();
 
@@ -53,7 +87,7 @@ var UIcontroller = (function () {
 })();
 
 
-var controller = (function (budgetCotrl, UICtrl) {
+var controller = (function (budgetCtrl, UICtrl) {
 
   var setupEventListeners = function () {
     var DOM = UICtrl.getDOMstrings();
@@ -68,11 +102,13 @@ var controller = (function (budgetCotrl, UICtrl) {
   };
 
   var ctrlAddItem = function () {
+    var input, newItem;
 
     // 1. Get the field input data
-    var input = UICtrl.getInput();
+    input = UICtrl.getInput();
 
     // 2. Add the item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
     // 3. Add the item to the UI
 
