c4b6ec3ecc7c0837b7195343bf90719b4a94c90d
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-03-02 16:19:07 +0900

    Lecture Closure

--- 05_advanced-JS/script.js
+++ 05_advanced-JS/script.js
@@ -171,3 +171,55 @@ game();
   console.log(score >= 5 - goodLuck);
 })(5);
 */
+
+// Lecuture:Closures
+
+function retirement(retirementAge) {
+  var a = ' years left until returement.';
+  return function (yearOfBirth) {
+    var age = 2016 - yearOfBirth;
+    console.log((retirementAge - age) + a);
+  }
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementUS(1990);
+retirementGermany(1990);
+retirementIceland(1990);
+
+// retirement(66)(1990);
+
+/*
+function interviewQuestion(job) {
+  if (job === 'designer') {
+    return function (name) {
+      console.log(name + ', can you please explain what UX design is?');
+    }
+  } else if (job === 'teacher') {
+    return function (name) {
+      console.log('What subject do you teach, ' + name + '?');
+    }
+  } else {
+    return function (name) {
+      console.log('Hello ' + name + 'what do you do?');
+    }
+  }
+}
+*/
+
+function interviewQuestion(job) {
+  return function (name) {
+    if (job === 'designer') {
+      console.log(name + ', can you please explain what UX design is?');
+    } else if (job === 'teacher') {
+      console.log('What subject do you teach, ' + name + '?');
+    } else {
+      console.log('Hello ' + name + ' what do you do?');
+    }
+  }
+}
+
+interviewQuestion('teacher')('John');
